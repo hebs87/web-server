3d6c00f9f27e2b159930bc2ce56aafa2a30e1381
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-11 20:00:41 +0100

    Created util function to write submitted form data to file and called in submit_form function

--- app.py
+++ app.py
@@ -16,6 +16,17 @@ def html_page(page_name):
     return render_template(page_name + '.html')
 
 
+def write_to_file(data):
+    """
+    A utility function to write the form data to a database.txt file
+    """
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        database.write(f'\n{email},{subject},{message}')
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     """
@@ -24,7 +35,7 @@ def submit_form():
     if request.method == 'POST':
         # Convert all form data into a dictionary
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou')
     else:
         return 'Something went wrong, please try again!'
