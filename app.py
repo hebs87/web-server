f8bb6b3b7151917a7910951eef39a6e39718071d
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-11 20:11:19 +0100

    Created util function to write submitted form data to csv file instead of txt file and called in submit_form function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -16,15 +17,27 @@ def html_page(page_name):
     return render_template(page_name + '.html')
 
 
-def write_to_file(data):
+# def write_to_file(data):
+#     """
+#     A utility function to write the form data to a database.txt file
+#     """
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
     """
-    A utility function to write the form data to a database.txt file
+    A utility function to write the form data to a database.csv file using the csv module
     """
-    with open('database.txt', mode='a') as database:
+    with open('database.csv', newline='', mode='a') as database:
         email = data['email']
         subject = data['subject']
         message = data['message']
-        database.write(f'\n{email},{subject},{message}')
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
@@ -35,7 +48,7 @@ def submit_form():
     if request.method == 'POST':
         # Convert all form data into a dictionary
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thankyou')
     else:
         return 'Something went wrong, please try again!'
