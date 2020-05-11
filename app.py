1730ce75d61a7de93d1cb11f7de531f6034033ff
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-05-11 19:37:47 +0100

    Created basic view for contact form submission and connected it to form in contact.html

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def html_page(page_name):
     return render_template(page_name + '.html')
 
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    """
+    A view to handle the contact form submission
+    """
+    return 'Form successfully submitted!'
+
+
 if __name__ == '__main__':
     app.run()
